291bb46a0c54984e6404b99edbbf52a0ee4ffc86
Wycliffe-Kim <f16o@hanyang.ac.kr>
2022-03-29 11:19:14 +0900

    change metric labels

--- promclient/metrics/Initializer.ts
+++ promclient/metrics/Initializer.ts
@@ -1,22 +1,17 @@
 import { Counter } from 'prom-client';
-import MetricTypes from './MetricTypes';
 
 export default function Initializer() {
-  function createCounter(type: MetricTypes) {
-    const _type = type.toLowerCase();
+  function createCounter() {
     new Counter({
-      name: `traffic_volumes_${_type}`,
-      help: `Traffic Volumes Metrics (${_type}) For Test`,
-      labelNames: ['camera_number']
+      name: 'traffic_volumes',
+      help: 'Traffic Volumes Metrics For Test',
+      labelNames: ['metric_type', 'camera_number']
     });
   }
 
   return {
     do() {
-      createCounter('CAR');
-      createCounter('BUS');
-      createCounter('TRUCK');
-      createCounter('MOTORCYCLE');
+      createCounter();
     }
   }
 }
--- promclient/metrics/TrafficVolumes.ts
+++ promclient/metrics/TrafficVolumes.ts
@@ -5,21 +5,13 @@ export default function TrafficVolumes(type: MetricTypes) {
   const _type = type.toLowerCase();
 
   function getCounter(camera_number: number) {
-    const _counter = register.getSingleMetric(`traffic_volumes_${_type}`) as Counter<string>;
-    return _counter.labels(`${camera_number}`);
-  }
-
-  async function check() {
-    const _dataForCheck = await register.getSingleMetricAsString(`traffic_volumes_${_type}`);
-    console.log('-----------------------');
-    console.log('check traffic volumes metric', _dataForCheck);
-    console.log('-----------------------');
+    const _counter = register.getSingleMetric('traffic_volumes') as Counter<string>;
+    return _counter.labels(_type, `${camera_number}`);
   }
 
   return {
     inc(camera_number: number, count: number) {
       getCounter(camera_number).inc(count);
-      // check();
     },
   }
 }
