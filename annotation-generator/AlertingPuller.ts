e1493c9ff2f33ac9dee4b0b4dcca1d50403f8c90
Wycliffe-Kim <f16o@hanyang.ac.kr>
2022-04-01 17:21:21 +0900

    correct docker files

--- annotation-generator/AlertingPuller.ts
+++ annotation-generator/AlertingPuller.ts
@@ -2,9 +2,10 @@ import axios from 'axios';
 import { WorkerAction } from './WorkerAction';
 import * as setting from '../setting.json';
 import { eitherCreator } from './functions';
-import { Alert } from './type';
+import { Alert, AlertType } from './type';
 
 const AlertingPuller: WorkerAction.WorkerFunc = (emitter) => {
+  type AlertMapFunc = () => Map<AlertType, Alert>;
   type EmitFunc = (eventName: string | symbol, ...args: any[]) => Promise<boolean>;
   type ParseFunc = (alert: any) => Alert;
 
@@ -15,14 +16,20 @@ const AlertingPuller: WorkerAction.WorkerFunc = (emitter) => {
   const start: WorkerAction.StartFunc = () => {
     setInterval(() => {
       axios.get(`http://${_address}:${_port}/api/v1/alerts`)
-      .then((response) => response.data.data.alerts)
-      .then((alerts: any[]) => {
+      .then((response) => response.data.data.alerts as any[])
+      .then((alerts) => {
         eitherCreator(alerts.length > 0, alerts, alerts)
         .map((alerts) => {
-          const _alerts = alerts.map((alert) => parse(alert));
-          console.log('--------------- AlertPuller ---------------');
-          console.log(JSON.stringify(_alerts));
-          emit('alert', _alerts);
+          const _alertMap = alertMap();
+          console.log('--------------- AlertPuller 1 ---------------');
+          console.log(JSON.stringify(_alertMap), alerts);
+          
+          alerts.map((alert) => parse(alert)).forEach((alert) => {
+            _alertMap.set(alert.type, alert);
+          });
+          console.log('--------------- AlertPuller 2 ---------------');
+          console.log(JSON.stringify(_alertMap));
+          // emit('alert', _alerts);
         })
       })
       .catch((error) => {
@@ -50,6 +57,16 @@ const AlertingPuller: WorkerAction.WorkerFunc = (emitter) => {
     value: alert.value,
     state: alert.state.toUpperCase()
   });
+
+  const alertMap: AlertMapFunc = () => {
+    const _alertMap = new Map<AlertType, Alert>();
+    _alertMap.set('CAR', Alert.defaultValue());
+    _alertMap.set('BUS', Alert.defaultValue());
+    _alertMap.set('TRUCK', Alert.defaultValue());
+    _alertMap.set('MOTORCYCLE', Alert.defaultValue());
+    console.log('alertMap', _alertMap);
+    return _alertMap;
+  }
 
   return {
     start
