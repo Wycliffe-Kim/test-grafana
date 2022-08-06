9282e0cf3e33eea92f525648be38d3bb90323cca
Wycliffe-Kim <f16o@hanyang.ac.kr>
2022-08-06 18:53:10 +0900

    remove job and instance

--- promclient/@types/TrafficCounters.ts
+++ promclient/@types/TrafficCounters.ts
@@ -6,8 +6,6 @@ export type TrafficCounters = {
   cameraId: string;
   flowType: FlowType;
   id: string;
-  instance: string;
-  job: string;
   linkId: string;
   objectType: string;
   siteId: string;
@@ -19,8 +17,6 @@ export namespace TrafficCounters {
     const cameraIds = _.range(4).map((i) => `camera${i}`);
     const flowTypes: FlowType[] = ['OUTFLOW', 'INFLOW'];
     const ids = _.range(4).map((i) => `id${i}`);
-    const instances = _.range(4).map((i) => `instance${i}`);
-    const jobs = _.range(4).map((i) => `job${i}`);
     const linkIds = _.range(8).map((i) => `link${i}`);
     const objectTypes = _.range(10).map((i) => `object${i}`);
     const siteIds = _.range(5).map((i) => `site${i}`);
@@ -29,8 +25,6 @@ export namespace TrafficCounters {
       cameraId: cameraIds[generateRandomNumber(0, cameraIds.length - 1)],
       flowType: flowTypes[generateRandomNumber(0, flowTypes.length - 1)],
       id: ids[generateRandomNumber(0, ids.length - 1)],
-      instance: instances[generateRandomNumber(0, instances.length - 1)],
-      job: jobs[generateRandomNumber(0, jobs.length - 1)],
       linkId: linkIds[generateRandomNumber(0, linkIds.length - 1)],
       objectType: objectTypes[generateRandomNumber(0, objectTypes.length - 1)],
       siteId: siteIds[generateRandomNumber(0, siteIds.length - 1)],
--- promclient/@types/TrafficGauges.ts
+++ promclient/@types/TrafficGauges.ts
@@ -15,8 +15,6 @@ export namespace TrafficGauges {
     const cameraIds = _.range(4).map((i) => `camera${i}`);
     const flowTypes: FlowType[] = ['OUTFLOW', 'INFLOW'];
     const ids = _.range(4).map((i) => `id${i}`);
-    const instances = _.range(4).map((i) => `instance${i}`);
-    const jobs = _.range(4).map((i) => `job${i}`);
     const linkIds = _.range(8).map((i) => `link${i}`);
     const objectTypes = _.range(10).map((i) => `object${i}`);
     const siteIds = _.range(5).map((i) => `site${i}`);
@@ -26,8 +24,6 @@ export namespace TrafficGauges {
       cameraId: cameraIds[generateRandomNumber(0, cameraIds.length - 1)],
       flowType: flowTypes[generateRandomNumber(0, flowTypes.length - 1)],
       id: ids[generateRandomNumber(0, ids.length - 1)],
-      instance: instances[generateRandomNumber(0, instances.length - 1)],
-      job: jobs[generateRandomNumber(0, jobs.length - 1)],
       linkId: linkIds[generateRandomNumber(0, linkIds.length - 1)],
       objectType: objectTypes[generateRandomNumber(0, objectTypes.length - 1)],
       siteId: siteIds[generateRandomNumber(0, siteIds.length - 1)],
